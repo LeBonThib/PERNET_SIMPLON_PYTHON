2bf185bb24f626d82765a9f4f75872c1e5ec7c11
Thibaut PERNET <thibaut.pernetx@gmail.com>
2022-01-18 14:47:55 +0100

    removed test file

--- projet_groupe_6/test2.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-alimconfiance_dataset_clean_raw_import = pd.read_csv('./datasets/export_alimconfiance_clean_raw_import.csv', sep=',')
-agrements = alimconfiance_dataset_clean_raw_import['agrement_etablissement'].astype(str)
-
-rot = alimconfiance_dataset_clean_raw_import.iloc[[0]]
-newdf = pd.DataFrame(rot)
-print(newdf)
