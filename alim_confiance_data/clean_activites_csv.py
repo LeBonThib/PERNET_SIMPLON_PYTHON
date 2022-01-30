a7deb18a8f1caad41b5a8c1d4b632ba70f1a6620
Thibaut PERNET <thibaut.pernetx@gmail.com>
2022-01-30 02:33:54 +0100

    qda

--- alim_confiance_data/clean_activites_csv.py
+++ alim_confiance_data/clean_activites_csv.py
@@ -14,17 +14,14 @@ for i in range(0,loop_length):
     row = alimconfiance_dataset_raw.iloc[i]
     industry = str(row['APP_Libelle_activite_etablissement'])
     industry_split = industry.split(industry_separator)
-    if len(industry_split) > 1:
-    #if industry.find(industry_separator):    
+    if len(industry_split) > 1:  
         for industry_single in industry_split:
-            industry_single = industry_split[0]
             row['APP_Libelle_activite_etablissement'] = industry_single.format(row['APP_Libelle_activite_etablissement'])
             dataframe_test = pd.DataFrame(row)
             dataframe_test = pd.DataFrame.transpose(dataframe_test)
             alimconfiance_dataset_raw = alimconfiance_dataset_raw.append(dataframe_test)
-            industry_split.pop(0)
             row['APP_Libelle_activite_etablissement'] = np.nan
 
 alimconfiance_dataset_raw.dropna(inplace=True)
 print(len(alimconfiance_dataset_raw)) 
-#alimconfiance_dataset_raw.to_csv('./datasets/test.csv', index=False)      
+alimconfiance_dataset_raw.to_csv('./datasets/test.csv', index=False)
